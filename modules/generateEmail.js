8da051961071706ffc2f74d57e07678e8b630f13
Summaw <98126132+Summaw@users.noreply.github.com>
2023-04-03 15:33:16 -0400

    Update generateEmail.js

--- modules/generateEmail.js
+++ modules/generateEmail.js
@@ -1,6 +1,12 @@
 const axios = require('axios');
+const fs = require('fs');
 
 const generateEmail = async () => {
+  const cookie = await axios.get('https://www.emailnator.com/');
+  const XSFR = await cookie.headers['set-cookie'][0].split(';')[0];
+  const gmailnator_session = await cookie.headers['set-cookie'][1].split(';')[0];
+  const x_xsfr_token = XSFR.split('=')[1].split('%')[0];
+
     const GenerateEmail = await axios.post(
         'https://www.emailnator.com/generate-email',
         {
@@ -12,7 +18,7 @@ const generateEmail = async () => {
           headers: {
             'authority': 'www.emailnator.com',
             'accept-language': 'en-US,en;q=0.9',
-            'cookie': 'XSRF-TOKEN=eyJpdiI6IkNYQ2p6bGkvUzVlMG1tbk5aZFdOSUE9PSIsInZhbHVlIjoia3VWZmZOVXhZaXFYaHM2eEpjRVZRbTF4ZW1DUEY5V3F0WkpPbzVjUkowRXJUNldvMXphMHI4Vmc2ZlM1WDV6dFYvMHpQZ0taanllZ09HTHlYdDdFK05GTzBraXo5Q3FmS3BZcEQ4cEMzZWdVK2RzendvYUxTU3Y4dTU0M1hUcEsiLCJtYWMiOiI1NzEzZWZkMDUxZDNmOTE1YjQxN2Y0YjJlMzdjMmIzYWQzZDdkOGU4N2JjOWM2NGFkNzVjMDZmNDY1MjI5OTk2IiwidGFnIjoiIn0%3D; gmailnator_session=eyJpdiI6Iit6dWpoalhPMndHSXBGaXJ1N3crdXc9PSIsInZhbHVlIjoibUJJNlVOTWJFMFB1citCaVFjZ1dXUFZBSHEyLzlrdW9VWm5SSTZYV3o0YWM2cHJPOVQwZlNYcUhXRHlBV1ozWkNieFFuMkxkRnJMaXNvSStuVmpHR0xOZE5XbHhoTUd5U3gvSFkvTkoxcTNOWFRKWlQrSTM0Si9xNEphNk1DSTgiLCJtYWMiOiIzM2Y4NzE2ZmI3NjJiN2I2ZDMzZDk1M2U5NzA1ZWEzMTJiZThmNjYxYjcyZjk2NGQ0M2JhZDQ5MjAxN2Y0MTc0IiwidGFnIjoiIn0%3D',
+            'cookie': `${XSFR}; ${gmailnator_session}`,
             'origin': 'https://www.emailnator.com',
             'referer': 'https://www.emailnator.com/',
             'sec-ch-ua': '"Not=A?Brand";v="8", "Chromium";v="110", "Opera GX";v="96"',
@@ -23,11 +29,19 @@ const generateEmail = async () => {
             'sec-fetch-site': 'same-origin',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 OPR/96.0.0.0',
             'x-requested-with': 'XMLHttpRequest',
-            'x-xsrf-token': 'eyJpdiI6IkQ0Q1R1MWtrWFBCOVhrbS9saHNpOXc9PSIsInZhbHVlIjoiNXFoRjFlSlNpRFYzVVZwR210aFRPcmhXVDhIRU5mbHF4VWlBVlpTZ2kvakhDRlR5Q3NkTlZRcCtMVXZIYm01bXVNa1VhRjU3THRuRFh0UzJVNEliK0dmMlNQZXFvVytRYTBCZDF1cHlqd3FqdVd3Z2lHWXcxUGZzME83YjNiSWkiLCJtYWMiOiIxMzAzYWZiYzI3OTBmNjU1NTlkNDAxODExMTg3ZDU3YjAyZTZkZjFhYzllNjIzOGRiY2NiMzcwZGVlN2IxZmNhIiwidGFnIjoiIn0='
+            'x-xsrf-token': `${x_xsfr_token}=`
           }
         });
-    let GeneratedEmail = GenerateEmail.data.email[0];
-    return GeneratedEmail;
+      let obj = {
+        email: GenerateEmail.data.email[0],
+        cookie: `${XSFR}; ${gmailnator_session}`,
+        x_xsfr_token: `${x_xsfr_token}=`
+      }
+      /*
+        comment the object above and uncomment the line below if you want to just generate the email not access it later on.
+      */
+    // let GeneratedEmail = GenerateEmail.data.email[0];
+    return obj;
 };
 
 module.exports = generateEmail;
